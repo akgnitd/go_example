06e7b082e22889a092acf8d2942826007f27fc05
atul <atul.gupta@creditsaison-in.com>
2022-07-20 23:57:51 +0530

    push

--- hello/hello.go
+++ hello/hello.go
@@ -7,9 +7,9 @@ func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
 	fmt.Println(fmt.Sprintf("Hi, %v. Welcome!", Hello("Atul")))
+	fmt.Println(Hello("Atul"))
 }
 
 func Hello(name string) string {
-	var ts string = "h"
 	return name
 }
